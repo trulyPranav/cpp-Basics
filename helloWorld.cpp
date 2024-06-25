6dbf637b81ccfd96433154d5c50b1d613a0339b6
Pranav M <pranavm265@gmail.com>
2024-06-25 20:55:00 +0530

    lesson 1

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World! Buahahahahahahhaha";
+/* int main(){
+    int x,y;
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+    cout << "Value of x is: " << x << endl << "And value of y is: " << y;
+    return 0;
+} */
+
+int main(){
+    // int 
+    int x = 1500;
+    // long
+    // long x = 1500000000000;
+    // long long
+    // long long x = 150000000000000000000;
+    // float & double
+    float y = 5.29376;
+    cout << "Value of y: " << y << endl;
+
+    // string & getline
+    string s;
+    getline (cin, s); // getline takes the entire line! while cin take only one word per string
+    cout << s;
     return 0;
 }
